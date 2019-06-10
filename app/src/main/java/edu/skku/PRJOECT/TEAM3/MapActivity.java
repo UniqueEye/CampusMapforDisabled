8c5636f8ca4ba0f8fa528172e8101d8dafd30c41
pleiadex <rudobco0825@gmail.com>
2019-06-10 15:56:21 +0900

    floating 버튼 삭제

--- app/src/main/java/edu/skku/PRJOECT/TEAM3/MapActivity.java
+++ app/src/main/java/edu/skku/PRJOECT/TEAM3/MapActivity.java
@@ -56,7 +56,6 @@ import java.util.Iterator;
 public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,OnMapReadyCallback{//sungyoun_브랜치
 
-    private int counter = 0;
     private FirebaseAuth mAuth;
     private static final String TAG = "MapActivity";
     private DatabaseReference store_mPostReference, building_mPostReference;
@@ -64,8 +63,6 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
     public double longitude;
     public double latitude;
     public double altitude;
-    public static final int NOTI_ID = 999;
-    private Marker currentMarker = null;
     StorePost store_post = new StorePost();
     BuildingPost building_post = new BuildingPost();
     ArrayList<String> building_id = new ArrayList<>();
@@ -113,53 +110,6 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
             Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
         }
 
-/*
-        FloatingActionButton button= findViewById(R.id.floatingActionButton);
-        final LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                if(currentMarker != null) currentMarker.remove();
-                Log.d("Current location", "Here");
-
-                LatLng default_location = new LatLng(37.293918, 126.975426);
-                LatLng my_loc = new LatLng(latitude, longitude);
-                Log.d("current location", String.valueOf(latitude) + ": "+String.valueOf(longitude));
-                if(latitude == 0){
-                    gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(default_location, 20));
-                }
-                BitmapDrawable bitmapdraw = (BitmapDrawable)getResources().getDrawable(R.drawable.current_location_pin);
-                Bitmap b=bitmapdraw.getBitmap();
-                Bitmap smallMarker = Bitmap.createScaledBitmap(b, 50, 50, false);
-                currentMarker = gmap.addMarker(new MarkerOptions()
-                        .position(my_loc)
-                        .icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
-
-                //Log.d("Counter", String.valueOf(counter));
-                if(latitude == 0) {
-                    gmap.moveCamera(CameraUpdateFactory.newLatLng(default_location));
-                    gmap.animateCamera(CameraUpdateFactory.zoomTo(17));
-                }
-                else {
-                    gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(my_loc, 20));
-                }
-
-
-                if ( Build.VERSION.SDK_INT >= 23 &&
-                        ContextCompat.checkSelfPermission( getApplicationContext(),
-                                android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-                    ActivityCompat.requestPermissions( MapActivity.this, new String[]
-                            { android.Manifest.permission.ACCESS_FINE_LOCATION },0 );
-                }
-                else{
-                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,0, gpsLocationListener);
-                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100, 0, networkLocationListener);
-                }
-            }
-        });
-        */
         building_getFirebaseDatabase();
         store_getFirebaseDatabase();
     }
